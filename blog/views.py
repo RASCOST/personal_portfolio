8ffb2d246b8cfa7e109eafe83898c4ff1c2d5ca0
Ricardo Costa <ra.silva.dcosta@gmail.com>
2022-01-31 20:09:17 +0100

    created detail view, added static image to portfolio, pluralize posts

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Blog
 
 def all_blogs(request):
     posts = Blog.objects.all()
 
     return render(request, 'blog/all_blogs.html', { 'posts': posts })
+
+def detail(request, blog_id):
+    post = get_object_or_404(Blog, pk=blog_id)
+    return render(request, 'blog/detail.html', {'post':post})
